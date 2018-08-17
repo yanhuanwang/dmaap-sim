d7cae3738ca1f1e18a66f2ca8d15450cbfc6849a
ecaiyan <martin.c.yan@est.tech>
2018-08-17 02:04:24 +0200

    Print request from DR

--- dmaapDR.js
+++ dmaapDR.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var bodyParser = require('body-parser')
 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/octet-stream' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
 app.post('/events/unauthenticated.FILE_READY', function (req, res) {
+	console.log(req.body)
+	console.log(req.headers)
    res.send("ok")
 })
 
--- dmaapMR.js
+++ dmaapMR.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require("fs");
 
 app.get('/events/unauthenticated.SEC_OTHER_OUTPUT/OpenDcae-c12/c12', function (req, res) {
-   res.json({"event":{"notificationFields": {"changeIdentifier": "PM_MEAS_FILES","changeType": "FileReady","notificationFieldsVersion": 1.0,"arrayOfAdditionalFields": [{"location": "ftpes://192.168.0.101:22/ftp/rop/A20161224.1030-1045.bin.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"},{"location": "ftpes://192.168.0.101:22/ftp/rop/A20161224.1045-1100.bin.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"}]}}})
+   res.json({"event":{"notificationFields": {"changeIdentifier": "PM_MEAS_FILES","changeType": "FileReady","notificationFieldsVersion": 1.0,"arrayOfAdditionalFields": [{"location": "ftpes://myuser:mypass@localhost:21/test5.txt","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"},{"location": "sftp://foo:pass@localhost:22/test3.txt","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"}]}}})
 })
 
 var server = app.listen(2222, function () {
