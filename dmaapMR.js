ff6cc306549e2488ddc8651966c40b7192c08274
yanhuanwang <martin.yan.seu@gmail.com>
2018-09-06 16:15:47 +0200

    add dmaap.py

--- dmaapMR.js
+++ dmaapMR.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/events/unauthenticated.SEC_OTHER_OUTPUT/OpenDcae-c12/c12', function (req, res) {
+app.get('/events/unauthenticated.FILE_READY/OpenDcae-c12/c12', function (req, res) {
 	var message={"event":{"notificationFields": {"changeIdentifier": "PM_MEAS_FILES","changeType": "FileReady","notificationFieldsVersion": 1.0,"arrayOfAdditionalFields": [{"location": "ftpes://myuser:mypass@localhost:21/fileFromFtps.tar.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"},{"location": "sftp://foo:pass@localhost:22/fileFromSftp.tar.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"}]}}}
 //	console.log("recieved request from DFC, response with message {\"event\":{\"notificationFields\": {\"changeIdentifier\": \"PM_MEAS_FILES\",\"changeType\": \"FileReady\",\"notificationFieldsVersion\": 1.0,\"arrayOfAdditionalFields\": [{\"location\": \"ftpes://myuser:mypass@localhost:21/fileFromFtps.tar.gz\",\"compression\": \"gzip\",\"fileFormatType\": \"org.3GPP.32.435#measCollec\",\"fileFormatVersion\": \"V10\"},{\"location\": \"sftp://foo:pass@localhost:22/fileFromSftp.tar.gz\",\"compression\": \"gzip\",\"fileFormatType\": \"org.3GPP.32.435#measCollec\",\"fileFormatVersion\": \"V10\"}]}}}")
 	console.log("recieved request from DFC, respond with message:")
