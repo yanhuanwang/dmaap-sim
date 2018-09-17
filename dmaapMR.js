ce7d016e35ec314dc88329473139cf7f798da392
yanhuanwang <martin.yan.seu@gmail.com>
2018-09-17 10:57:17 +0200

    arrayOfNamedHashMap

--- dmaapMR.js
+++ dmaapMR.js
@@ -3,7 +3,19 @@ var app = express();
 var fs = require("fs");
 
 app.get('/events/unauthenticated.VES_NOTIFICATION_OUTPUT/OpenDcae-c12/C12', function (req, res) {
-	var message={"event":{"notificationFields": {"changeIdentifier": "PM_MEAS_FILES","changeType": "FileReady","notificationFieldsVersion": 1.0,"arrayOfAdditionalFields": [{"location": "ftpes://myuser:mypass@localhost:21/fileFromFtps.tar.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"},{"location": "sftp://foo:pass@localhost:22/fileFromSftp.tar.gz","compression": "gzip","fileFormatType": "org.3GPP.32.435#measCollec","fileFormatVersion": "V10"}]}}}
+	var message={"event":{"notificationFields": {"changeIdentifier": "PM_MEAS_FILES","changeType": "FileReady","notificationFieldsVersion": 1.0,"arrayOfNamedHashMap": [{"name": "fileFromFtps.tar.gz", "hashMap": {
+          "location": "ftpes://myuser:mypass@localhost:21/fileFromFtps.tar.gz",
+          "compression": "gzip",
+          "fileformatType": "org.3GPP.32.435#measCollec",
+          "fileFormatVersion": "V10"
+        }
+      },{"name": "fileFromSftp.tar.gz", "hashMap": {
+          "location": "sftp://foo:pass@localhost:22/fileFromSftp.tar.gz",
+          "compression": "gzip",
+          "fileformatType": "org.3GPP.32.435#measCollec",
+          "fileFormatVersion": "V10"
+        }
+      }]}}}
 //	console.log("recieved request from DFC, response with message {\"event\":{\"notificationFields\": {\"changeIdentifier\": \"PM_MEAS_FILES\",\"changeType\": \"FileReady\",\"notificationFieldsVersion\": 1.0,\"arrayOfAdditionalFields\": [{\"location\": \"ftpes://myuser:mypass@localhost:21/fileFromFtps.tar.gz\",\"compression\": \"gzip\",\"fileFormatType\": \"org.3GPP.32.435#measCollec\",\"fileFormatVersion\": \"V10\"},{\"location\": \"sftp://foo:pass@localhost:22/fileFromSftp.tar.gz\",\"compression\": \"gzip\",\"fileFormatType\": \"org.3GPP.32.435#measCollec\",\"fileFormatVersion\": \"V10\"}]}}}")
 	console.log("recieved request from DFC, respond with message:")
 	console.log(JSON.stringify(message, undefined, 4));
