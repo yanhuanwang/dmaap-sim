8257cedc05561fefccf42be025a416b423dd13c5
yanhuanwang <martin.yan.seu@gmail.com>
2018-09-26 09:36:57 +0200

    write file in local disk

--- dmaapDR.js
+++ dmaapDR.js
@@ -2,8 +2,15 @@ var express = require('express');
 const stream = require('stream');
 var app = express();
 var fs = require("fs");
-var bodyParser = require('body-parser')
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+});
+const fileUpload = require('express-fileupload');
+var path = require('path');
 
+var bodyParser = require('body-parser')
+app.use(fileUpload());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,14 +26,20 @@ app.use(bodyParser.raw({ type: 'application/octet-stream' }))
 
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
-app.post('/events/unauthenticated.FILE_READY', function (req, res) {
+app.post('/publish/1/:filename', function (req, res) {
 	console.log(req.files);
 	console.log(req.body)
 	console.log(req.headers)
-   res.send("ok")
+	var filename = path.basename(req.params.filename);
+  filename = path.resolve(__dirname, filename);
+	console.log(req.params.filename);
+  fs.writeFile(filename, req.body, function (error) {
+  	if (error) { console.error(error); }
+	});
+	 res.send("ok")
 })
 
-var server = app.listen(3905, function () {
+var server = app.listen(3906, function () {
 
   var host = server.address().address
   var port = server.address().port
